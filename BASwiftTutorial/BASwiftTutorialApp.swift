c24eb5e0b664ac7387578fb04a3736c00936f0c2
yakintech <cagatayyildiz87@gmail.com>
2021-09-26 10:13:58 +0300

    Initial Commit

---
+++ BASwiftTutorial/BASwiftTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  BASwiftTutorialApp.swift
+//  BASwiftTutorial
+//
+//  Created by Çağatay Yıldız on 26.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct BASwiftTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
